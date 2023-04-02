c64c7480651debbd9bba0625863b3c360876fcdf
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-04-02 09:25:01 +0200

    add delete post function with modal

--- src/components/pages/SinglePost/SinglePost.js
+++ src/components/pages/SinglePost/SinglePost.js
@@ -1,4 +1,5 @@
-import { Button, Container } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Container, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
 import { deletePost, getPostById } from "../../../redux/postsRedux";
@@ -6,13 +7,21 @@ import { deletePost, getPostById } from "../../../redux/postsRedux";
 const SinglePost = () => {
 
   const dispatch = useDispatch();
-
-  const handleDelete  = () => {
-    dispatch(deletePost(id));
-  };
-
   const { id } = useParams();
   const postData = useSelector(state => getPostById(state, id));
+
+  // modal state
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
+  const handleDeletePost  = (e) => {
+    e.preventDefault();
+    dispatch(deletePost(id));
+    handleCloseModal();
+  };
+
 
   if(!postData) return <Navigate to="/" />
     return (
@@ -21,11 +30,30 @@ const SinglePost = () => {
           <h2>{postData.title}</h2>
           <div>
             <Button href="/post/edit" variant="outline-info" className="me-2">Edit Post</Button>
-            <Button onClick={handleDelete} variant="outline-danger">Delete Post</Button>
+            <Button onClick={handleShowModal} variant="outline-danger">Delete Post</Button>
           </div>
         </Container>
         <p>Author: {postData.author}</p>
         <p>{postData.content}</p>
+
+        {/* Modal dialog */}
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>This operation will completely remove this post from the app</p>
+            <p>Are you sure you want to do that?</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleDeletePost}>
+              Delete Post
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
 };
