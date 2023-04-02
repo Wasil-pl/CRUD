dd2c522abc4e120d9f47d59cd24048369919120a
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-04-02 09:10:31 +0200

    add DeletePost function

--- src/components/pages/SinglePost/SinglePost.js
+++ src/components/pages/SinglePost/SinglePost.js
@@ -1,8 +1,15 @@
-import { useSelector } from "react-redux";
+import { Button, Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
-import { getPostById } from "../../../redux/postsRedux";
+import { deletePost, getPostById } from "../../../redux/postsRedux";
 
 const SinglePost = () => {
+
+  const dispatch = useDispatch();
+
+  const handleDelete  = () => {
+    dispatch(deletePost(id));
+  };
 
   const { id } = useParams();
   const postData = useSelector(state => getPostById(state, id));
@@ -10,7 +17,13 @@ const SinglePost = () => {
   if(!postData) return <Navigate to="/" />
     return (
       <div>
-        <h2>{postData.title}</h2>
+        <Container className="d-flex justify-content-between align-items-center mb-4">
+          <h2>{postData.title}</h2>
+          <div>
+            <Button href="/post/edit" variant="outline-info" className="me-2">Edit Post</Button>
+            <Button onClick={handleDelete} variant="outline-danger">Delete Post</Button>
+          </div>
+        </Container>
         <p>Author: {postData.author}</p>
         <p>{postData.content}</p>
       </div>
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -3,11 +3,16 @@ export const getAllPosts = ( state ) => state.posts
 export const getPostById = ({ posts }, postId) => posts.find(post => post.id === postId);
 
 // actions
+export const deletePost = payload => ({ type: DELETE_POST, payload });
+
 const createActionName = actionName => `app/posts/${actionName}`;
+const DELETE_POST = createActionName('DELETE_POST');
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case DELETE_POST:
+      return statePart.filter(post => post.id !== action.payload);
     default:
       return statePart;
   };
