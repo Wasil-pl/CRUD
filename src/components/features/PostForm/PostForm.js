f762ee3390c18f8d5ee1f6e23fb89ee7a8108130
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-04-07 18:21:26 +0200

    fix usestate name function

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -5,39 +5,30 @@ import { isValidDate } from '../../../utils/isValidDate';
 const PostForm = ({ pageTitle, action, actionText, ...props }) => {
   const [title, setTitle] = useState(props.title || '');
   const [author, setAuthor] = useState(props.author || '');
-  const [handlePublishedDateChange, setHandlePublishedDateChange] = useState(
-    props.publishedDate || ''
-  );
-  const [handlePublishedDateChangeError, setHandlePublishedDateChangeError] =
-    useState(null);
-  const [shortDescription, setShortDescription] = useState(
-    props.shortDescription || ''
-  );
+  const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
+  const [publishedDateError, setPublishedDateError] = useState(null);
+  const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
   const [content, setContent] = useState(props.content || '');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!isValidDate(handlePublishedDateChange)) {
-      alert(
-        'Invalid date format. Please enter a date in the format dd-mm-yyyy.'
-      );
+    if (!isValidDate(publishedDate)) {
+      alert('Invalid date format. Please enter a date in the format dd-mm-yyyy.');
       return;
     }
     action({
       title,
       author,
-      handlePublishedDateChange,
+      publishedDate,
       shortDescription,
       content,
     });
   };
 
-  const onChange = (e) => {
-    setHandlePublishedDateChange(e.target.value);
-    setHandlePublishedDateChangeError(
-      isValidDate(e.target.value)
-        ? null
-        : 'Invalid date format. Please enter a date in the format dd-mm-yyyy.'
+  const handlePublishedDateChange = (e) => {
+    setPublishedDate(e.target.value);
+    setPublishedDateError(
+      isValidDate(e.target.value) ? null : 'Invalid date format. Please enter a date in the format dd-mm-yyyy.'
     );
   };
 
@@ -45,12 +36,7 @@ const PostForm = ({ pageTitle, action, actionText, ...props }) => {
     <Form onSubmit={handleSubmit}>
       <Form.Group controlId="title" as={Col} md="4" className="mb-3">
         <Form.Label>Title</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+        <Form.Control type="text" placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)} />
       </Form.Group>
 
       <Form.Group controlId="author" as={Col} md="4" className="mb-3">
@@ -68,21 +54,14 @@ const PostForm = ({ pageTitle, action, actionText, ...props }) => {
         <Form.Control
           type="text"
           placeholder="Enter date: dd-mm-rrrr"
-          value={handlePublishedDateChange}
-          onChange={onChange}
-          isInvalid={handlePublishedDateChangeError !== null}
+          value={publishedDate}
+          onChange={handlePublishedDateChange}
+          isInvalid={publishedDateError !== null}
         />
-        <Form.Control.Feedback type="invalid">
-          {handlePublishedDateChangeError}
-        </Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">{publishedDateError}</Form.Control.Feedback>
       </Form.Group>
 
-      <Form.Group
-        controlId="shortDescription"
-        as={Col}
-        md="10"
-        className="mb-3"
-      >
+      <Form.Group controlId="shortDescription" as={Col} md="10" className="mb-3">
         <Form.Label>Short Description</Form.Label>
         <Form.Control
           type="text"
